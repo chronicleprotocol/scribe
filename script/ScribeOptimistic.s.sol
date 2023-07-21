41cdddd32cd35f8db2e9db0a9f87cfcb3031f856
pascal <pascal@merkleplant.xyz>
2023-07-21 10:13:13 +0200

    scripts/: Finish Scribe scripts for chronicles usage

--- script/ScribeOptimistic.s.sol
+++ script/ScribeOptimistic.s.sol
@@ -1,34 +1,45 @@
 pragma solidity ^0.8.16;
 
-import {Script} from "forge-std/Script.sol";
-import {console2} from "forge-std/console2.sol";
-
+import {IScribe} from "src/IScribe.sol";
 import {IScribeOptimistic} from "src/IScribeOptimistic.sol";
 import {ScribeOptimistic} from "src/ScribeOptimistic.sol";
+
+import {ScribeScript} from "./Scribe.s.sol";
 
 import {LibSecp256k1} from "src/libs/LibSecp256k1.sol";
 
 /**
  * @title ScribeOptimistic Management Script
- *
- * @dev Usage:
  */
-contract ScribeOptimisticScript is Script {
-    /// @dev Anvil's default mnemonic.
-    string internal constant ANVIL_MNEMONIC =
-        "test test test test test test test test test test test junk";
-
-    function deploy() public returns (IScribeOptimistic) {
-        uint deployer = vm.deriveKey(ANVIL_MNEMONIC, uint32(0));
-
-        vm.startBroadcast(deployer);
-        IScribeOptimistic opScribe = new ScribeOptimistic("ETH/USD");
-        vm.stopBroadcast();
-
-        return opScribe;
+abstract contract ScribeOptimisticScript is ScribeScript {
+    function opPoke(
+        address self,
+        IScribe.PokeData memory pokeData,
+        IScribe.SchnorrData memory schnorrData,
+        IScribe.ECDSAData memory ecdsaData
+    ) public {
+        vm.broadcast();
+        IScribeOptimistic(self).opPoke(pokeData, schnorrData, ecdsaData);
     }
 
-    function setBar() public {}
+    function opChallenge(address self, IScribe.SchnorrData memory schnorrData)
+        public
+    {
+        vm.broadcast();
+        IScribeOptimistic(self).opChallenge(schnorrData);
+    }
 
-    // ...
+    function setOpChallengePeriod(address self, uint16 opChallengePeriod)
+        public
+    {
+        vm.broadcast();
+        IScribeOptimistic(self).setOpChallengePeriod(opChallengePeriod);
+    }
+
+    function setMaxChallengeReward(address self, uint maxChallengeReward)
+        public
+    {
+        vm.broadcast();
+        IScribeOptimistic(self).setMaxChallengeReward(maxChallengeReward);
+    }
 }
